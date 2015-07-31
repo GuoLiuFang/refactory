2e74b6368705d652f03fd47d78eb757f4a7804de
guoliufang <guoliufang@tigerjoys.com>
2015-07-31 13:03:03 +0800

    Replace Conditional with Polymorphism

--- src/com/glf/elit/origin/ChildrensPrice.java
+++ src/com/glf/elit/origin/ChildrensPrice.java
@@ -8,4 +8,13 @@ public class ChildrensPrice extends Price {
     int getPriceCode() {
         return Movie.CHILDRENDS;
     }
+
+    @Override
+    public double getCharge(int daysRented) {
+        double result = 1.5;
+        if (daysRented > 3)
+            result += (daysRented -3) * 1.5;
+        return  result;
+
+    }
 }
--- src/com/glf/elit/origin/RegularPrice.java
+++ src/com/glf/elit/origin/RegularPrice.java
@@ -8,4 +8,16 @@ public class RegularPrice extends Price {
     int getPriceCode() {
         return Movie.REGULAR;
     }
+
+    @Override
+    public double getCharge(int daysRented) {
+//        return super.getCharge(daysRented);
+        double result = 2;
+        if (daysRented > 2)
+            result += (daysRented -2) * 1.5;
+        return result;
+    }
+    /**
+     * 终于对抽象类的用法有了点感觉了。原来如此呀！！！
+     */
 }
